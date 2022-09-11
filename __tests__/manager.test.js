40d8e04237661204e20fb24024868bffc7246328
Nunder <nathannunderwood@gmail.com>
2022-09-11 00:01:28 -0700

    manager.test.js all pass

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -2,16 +2,16 @@ const Manager = require('../lib/manager.js');
 
 describe("Manager", () => {
     it("Should create new Manager object", () => {
-        const manager = new Manager('Nathan', 1, 'nathanrolandpotter@yahoo.com');
+        const manager = new Manager('Nathan', 1, 'nathanrolandpotter@yahoo.com', 9);
 
-        expect(manager.getOfficeNumber().not.toBeNull());
+        expect(manager.officeNumber).toEqual(expect.any(Number));
     });
 
     describe("Manager Office Number", () => {
         it("Should get Manger Office Number from getOfficeNumber()", () => {
-            const manager = new Manager('Nathan', 1, 'nathanrolandpotter@yahoo.com');
+            const manager = new Manager('Nathan', 1, 'nathanrolandpotter@yahoo.com', 9);
 
-            expect(manager.getOfficeNumber().not.toBeNull());
+            expect(manager.getOfficeNumber()).toEqual(expect.any(Number));
         })  
     });
 
