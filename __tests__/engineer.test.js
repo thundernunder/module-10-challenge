0cc0aa6521fe9354fbf4cfae0dd7cb2029c27c13
Nunder <nathannunderwood@gmail.com>
2022-09-11 00:19:21 -0700

    All engineer.test.js pass

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,34 +1,26 @@
-const Engineeer = require('../lib/engineer');
+const Engineer = require('../lib/engineer');
 
-describe("Engineer"), () => {
+describe("Engineer", () => {
     it("Should create new Enginner object", () => {
-        const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com');
+        const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com', 'github');
 
-        expect(engineer.getGitub().not.toBeNull());
+        expect(engineer.github).not.toBeNull();
     });
 
     describe("Enginner GitHub", () => {
         it("Should get Enginner Github from gitHub()", () => {
-            const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com');
+            const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com', 'github');
 
-            expect(engineer.getGitub().not.toBeNull());
+            expect(engineer.getGithub()).toEqual(expect.any(String));
         })
         
     });
-
-    describe("Engineer GitHub", () => {
-        it("Should get Enginner Github from gitHub()", () => {
-            const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com');
-
-            expect(engineer.getGitub().not.toBeNull());
-        })
-    })
 
     describe('Get employee role', () => {
         it('Should be able to get employee role', () => {
             const engineer = new Engineer('Nathan', 1, 'nathanrolandpotter@yahoo.com');
 
-            expect(Engineer.getRole()).toEqual("Engineer");
+            expect(engineer.getRole()).toEqual("Engineer");
         });
     });
-}
+});
